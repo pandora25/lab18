96e4e244c8c193a46b082dac085bfdd6f216e45a
MHY Express <mcyalda@yahoo.com>
2018-05-17 08:57:03 -0400

    1st commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //part1
+
+            GoogleList google = new GoogleList();
+            google.Add("Mike");
+            google.Add("Mike");
+            google.Add("James");
+            google.Add("Anthony");
+            google.Add("Mike");
+            google.Add("John");
+            google.RemoveAll("Mike");
+            google.PrintInfo();
+
+
+            Console.WriteLine("part2:\n\n");
+            int[] numbers = { 1, 2, 3, 2, 2, 4, 5, 5, 7, 8, 4, 4, 1, 0, 10 };
+            int max = 0;
+
+
+            max = FindMax(numbers);
+            int[] freq = new int[max + 1];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                freq[numbers[i]]++;
+            }
+            for (int i = 0; i < freq.Length; i++)
+            {
+                if (freq[i] != 0)
+                {
+                    Console.WriteLine($"[{i}]:{freq[i]}");
+                }
+            }
+
+        }
+        public static int FindMax(int[] numbs)
+        {
+            int max = 0;
+            for (int i = 0; i < numbs.Length; i++)
+            {
+                if (numbs[i] > max)
+                {
+                    max = numbs[i];
+                }
+            }
+            return max;
+        }
+    }
+}
---
+++ TheMostAwesomeList.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab18
+{
+    class GoogleList
+    {
+        public Node Head { set; get; }
+        public Node Tail { set; get; }
+        public int Count { set; get; }
+
+        public void Add(string userInput)
+        {
+            Node node = new Node { Next = null, Data = userInput };
+            if (Count == 0)
+            {
+                Head = node;
+                Tail = node;
+                Count++;
+            }
+            else
+            {
+                Tail.Next = node;
+                Tail = node;
+                Count++;
+            }
+        }
+        public Node GetNode(int index) // get
+        {
+            int counter = 0;
+            Node node = Head;
+            while (counter != index)
+            {
+                node = node.Next;
+                counter++;
+            }
+            return node;
+        }
+        public void RemoveAll(string userInput) // remove the reference to the node to remove it. remove the .next
+        {
+            Node node = Head;
+            while (node.Data == userInput)
+            {
+                Head = Head.Next;
+                node = node.Next;
+                Count--;
+            }
+            while (node != null)
+            {
+                while (node.Next != null && node.Next.Data == userInput)
+                {
+                    node.Next = node.Next.Next;
+                    Count--;
+                }
+                node = node.Next;
+            }
+        }
+        public void Clear()
+        {
+            Count = 0;
+        }
+        public void PrintInfo()
+        {
+            Node node = Head;
+            while (node != null)
+            {
+                Console.WriteLine(node.Data);
+                node = node.Next;
+            }
+        }
+    }
+}
